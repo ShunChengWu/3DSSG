9a4df49e8d1631758c598cfe49a638c130d99a17
Shun-Cheng Wu <shuncheng.wu91@gmail.com>
2022-01-27 20:14:05 +0100

    fix bug

--- codeLib/__init__.py
+++ codeLib/__init__.py
@@ -1,10 +1,7 @@
-if __name__ == '__main__' and __package__ is None:
-    from os import sys
-    sys.path.append('../')
 from . import loggers
 from . import models
-from . import modules
-from . import nn
+#from . import modules
+#from . import nn
 from . import torch
 from . import utils
 from . import common
@@ -12,5 +9,5 @@ from .config import Config
 from . import object
 from . import transformation
 
-__all__ = ['loggers','models','modules','nn','torch','utils','common', 
+__all__ = ['loggers','models','torch','utils','common', 
            'Config', 'object','transformation']
