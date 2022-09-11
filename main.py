add upper boudn calculation. fix a indexing bug

main.py
@@ -26,7 +26,7 @@ logger_py = logging.getLogger(__name__)
 # logger_py.setLevel(logging.DEBUG)
 
 def main():
-    cfg = parse()
+    cfg = ssg.Parse()
     
     # Shorthands
     out_dir = os.path.join(cfg['training']['out_dir'], cfg.name)
@@ -168,15 +168,7 @@ def main():
             ''' Get segment dataset '''
             dataset_seg  = config.get_dataset(cfg,'test')
             ''' Get instance dataset'''
-            tmp_cfg = copy.deepcopy(cfg)
-            tmp_cfg.model.use_rgb = False
-            tmp_cfg.model.use_normal = False
-            tmp_cfg.data.input_type = 'sgfn'
-            tmp_cfg.data.load_images=False
-            tmp_cfg.data.load_points=False
-            tmp_cfg.data.path = cfg.data.path_gt
-            tmp_cfg.data.label_file = cfg.data.label_file_gt
-            dataset_inst  = config.get_dataset(tmp_cfg,'test')
+            dataset_inst = config.get_dataset_inst(cfg,'test')
             # write back
             
             '''check'''
@@ -208,7 +200,7 @@ def main():
             logger_py.info('start evaluation')
             pr = cProfile.Profile()
             pr.enable()    
-            eval_dict, eval_tool = model_trainer.evaluate_inst(dataset_seg,dataset_inst, topk=cfg.eval.topK)
+            eval_dict, eval_tool,eval_tool_upperbound = model_trainer.evaluate_inst(dataset_seg,dataset_inst, topk=cfg.eval.topK)
             pr.disable()
             logger_py.info('save time profile to {}'.format(os.path.join(out_dir,'tp_eval_inst.dmp')))
             pr.dump_stats(os.path.join(out_dir,'tp_eval_inst.dmp'))
@@ -219,6 +211,9 @@ def main():
             
             print(eval_tool.gen_text())
             _ = eval_tool.write(out_dir, prefix)
+            
+            eval_tool_upperbound.write(out_dir,'upper_bound')
+            
             if logger:
                 for k,v in eval_dict['visualization'].items(): 
                     logger.add_figure('test/'+prefix+'_'+k, v, global_step=it)
@@ -288,49 +283,6 @@ def main():
     else:
         raise NotImplementedError('unknown input mode')
         
-
-def parse():
-    r"""loads model config
-
-    """
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--config', type=str, default='./configs/default.yaml', help='configuration file name. Relative path under given path (default: config.yml)')
-    parser.add_argument('--mode', type=str, choices=['train','trace','eval','sample','trace'], default='train', help='mode. can be [train,trace,eval]',required=False)
-    parser.add_argument('--loadbest', type=int, default=0,choices=[0,1], help='1: load best model or 0: load checkpoints. Only works in non training mode.')
-    parser.add_argument('--log', type=str, default='DEBUG',choices=['DEBUG','INFO','WARNING','CRITICAL'], help='')
-    args = parser.parse_args()
-    
-    config_path = os.path.abspath(args.config)
-    if not os.path.exists(config_path):
-        raise RuntimeError('Targer config file does not exist. {}' & config_path)
-        
-    # load config file
-    config = codeLib.Config(config_path)
-    # return config
-    config.LOADBEST = args.loadbest
-    config.MODE = args.mode
-    
-    # check if name exist
-    if 'name' not in config:
-        config_name = os.path.basename(args.config)
-        if len(config_name) > len('config_'):
-            name = config_name[len('config_'):]
-            name = os.path.splitext(name)[0]
-            translation_table = dict.fromkeys(map(ord, '!@#$'), None)
-            name = name.translate(translation_table)
-            config['name'] = name 
-    
-    # init device
-    if torch.cuda.is_available() and len(config.GPU) > 0:
-        config.DEVICE = torch.device("cuda")
-    else:
-        config.DEVICE = torch.device("cpu")      
-        
-    config.log_level = args.log
-    # logging.basicConfig(level=config.log_level)
-    # logging.setLevel(config.log_level)
-    return config
-
 if __name__ == '__main__':
     # logger_py.setLevel('DEBUG')
     # logger_py.debug('hello0')

ssg/config.py
@@ -1,7 +1,6 @@
-import os
+import os,copy,logging
 import ssg
 from ssg import SSG3D, SGFN, SGPN, MVEnc, SVEnc, IMP#, DestCmp
-import logging
 from torch.utils.tensorboard import SummaryWriter
 from codeLib.loggers import WandbLogger
 from codeLib.common import filter_args_create
@@ -62,6 +61,18 @@ def get_dataset(cfg, mode='train'):
     if 'node_feature_dim' in cfg.model:
         cfg.data.node_feature_dim = cfg.model.node_feature_dim
     return ssg.dataset.dataset_dict[cfg.data.input_type](cfg, mode=mode)
+
+def get_dataset_inst(cfg,mode='test'):
+    tmp_cfg = copy.deepcopy(cfg)
+    tmp_cfg.model.use_rgb = False
+    tmp_cfg.model.use_normal = False
+    tmp_cfg.data.input_type = 'sgfn'
+    tmp_cfg.data.load_images=False
+    tmp_cfg.data.load_points=False
+    tmp_cfg.data.path = cfg.data.path_gt
+    tmp_cfg.data.label_file = cfg.data.label_file_gt
+    dataset_inst  = get_dataset(tmp_cfg,mode)
+    return dataset_inst
 
 def get_model(cfg, num_obj_cls,num_rel_cls):
     ''' Returns the model instance.

ssg/trainer/trainer_IMP.py
@@ -4,14 +4,14 @@ from collections import defaultdict
 from codeLib.models import BaseTrainer
 from codeLib.common import check_weights, convert_torch_to_scalar
 import torch
-from ssg.utils.util_eva import EvalSceneGraph
+from ssg.utils.util_eva import EvalSceneGraph, EvalUpperBound
 import time
 import logging
 import codeLib.utils.moving_average as moving_average 
 import ssg
 import codeLib.utils.string_numpy as snp
 from ssg import define
-from ssg.utils.util_data import merge_batch_mask2inst
+from ssg.utils.util_data import merge_batch_mask2inst,match_class_info_from_two
 
 logger_py = logging.getLogger(__name__)
 
@@ -80,49 +80,61 @@ class Trainer_IMP(BaseTrainer):
     def evaluate_inst(self,dataset_seg,dataset_inst,topk):
         ignore_missing=self.cfg.eval.ignore_missing
         '''add a none class for missing instances'''
-        node_cls_names = copy.copy(self.node_cls_names)
-        edge_cls_names = copy.copy(self.edge_cls_names)
-        if define.NAME_NONE not in self.node_cls_names:
-            node_cls_names.append(define.NAME_NONE)
-        if define.NAME_NONE not in self.edge_cls_names:
-            edge_cls_names.append(define.NAME_NONE)
-        # remove same part
-        # samepart_idx_edge_cls = self.edge_cls_names.index(define.NAME_SAME_PART)
-        if define.NAME_SAME_PART in edge_cls_names:
-            edge_cls_names.remove(define.NAME_SAME_PART)
         
-        noneidx_node_cls = node_cls_names.index(define.NAME_NONE)
-        noneidx_edge_cls = edge_cls_names.index(define.NAME_NONE)
+        (scanid2idx_seg, node_cls_names, edge_cls_names,noneidx_node_cls,noneidx_edge_cls,
+            seg_valid_node_cls_indices,inst_valid_node_cls_indices,
+            seg_valid_edge_cls_indices,inst_valid_edge_cls_indices) = \
+            match_class_info_from_two(dataset_seg,dataset_inst)
         
-        '''
-        Find index mapping. Ignore NONE for nodes since it is used for mapping missing instance.
-        Ignore SAME_PART for edges.
-        '''
-        seg_valid_node_cls_indices = []
-        inst_valid_node_cls_indices = []
-        for idx in range(len(self.node_cls_names)):
-            name = self.node_cls_names[idx]
-            if name == define.NAME_NONE: continue
-            seg_valid_node_cls_indices.append(idx)
-        for idx in range(len(node_cls_names)):
-            name = node_cls_names[idx]
-            if name == define.NAME_NONE: continue
-            inst_valid_node_cls_indices.append(idx)
+        # node_cls_names = copy.copy(self.node_cls_names)
+        # edge_cls_names = copy.copy(self.edge_cls_names)
+        # if define.NAME_NONE not in self.node_cls_names:
+        #     node_cls_names.append(define.NAME_NONE)
+        # if define.NAME_NONE not in self.edge_cls_names:
+        #     edge_cls_names.append(define.NAME_NONE)
+        # # remove same part
+        # # samepart_idx_edge_cls = self.edge_cls_names.index(define.NAME_SAME_PART)
+        # if define.NAME_SAME_PART in edge_cls_names:
+        #     edge_cls_names.remove(define.NAME_SAME_PART)
         
-        seg_valid_edge_cls_indices = []
-        inst_valid_edge_cls_indices = []
-        for idx in range(len(self.edge_cls_names)):
-            name = self.edge_cls_names[idx]
-            if name == define.NAME_SAME_PART: continue
-            seg_valid_edge_cls_indices.append(idx)
-        for idx in range(len(edge_cls_names)):
-            name = edge_cls_names[idx]
-            if name == define.NAME_SAME_PART: continue
-            inst_valid_edge_cls_indices.append(idx)
+        # noneidx_node_cls = node_cls_names.index(define.NAME_NONE)
+        # noneidx_edge_cls = edge_cls_names.index(define.NAME_NONE)
+        
+        # '''
+        # Find index mapping. Ignore NONE for nodes since it is used for mapping missing instance.
+        # Ignore SAME_PART for edges.
+        # '''
+        # seg_valid_node_cls_indices = []
+        # inst_valid_node_cls_indices = []
+        # for idx in range(len(self.node_cls_names)):
+        #     name = self.node_cls_names[idx]
+        #     if name == define.NAME_NONE: continue
+        #     seg_valid_node_cls_indices.append(idx)
+        # for idx in range(len(node_cls_names)):
+        #     name = node_cls_names[idx]
+        #     if name == define.NAME_NONE: continue
+        #     inst_valid_node_cls_indices.append(idx)
+        
+        # seg_valid_edge_cls_indices = []
+        # inst_valid_edge_cls_indices = []
+        # for idx in range(len(self.edge_cls_names)):
+        #     name = self.edge_cls_names[idx]
+        #     if name == define.NAME_SAME_PART: continue
+        #     seg_valid_edge_cls_indices.append(idx)
+        # for idx in range(len(edge_cls_names)):
+        #     name = edge_cls_names[idx]
+        #     if name == define.NAME_SAME_PART: continue
+        #     inst_valid_edge_cls_indices.append(idx)
         
         
         eval_tool = EvalSceneGraph(node_cls_names, edge_cls_names,multi_rel_prediction=self.cfg.model.multi_rel,k=topk,save_prediction=True,
                                    none_name=define.NAME_NONE) 
+        
+        eval_UpperBound = EvalUpperBound(node_cls_names,edge_cls_names,noneidx_node_cls,noneidx_edge_cls,
+                                         multi_rel=self.cfg.model.multi_rel,topK=topk,none_name=define.NAME_NONE)
+        
+        
+        # eval_upper_bound = 
         eval_list = defaultdict(moving_average.MA)
         
         '''check'''
@@ -142,18 +154,8 @@ class Trainer_IMP(BaseTrainer):
         for index in range(len(dataset_inst)):
             scan_id = snp.unpack(dataset_inst.scans,index)# self.scans[idx]
             scanid2idx_inst[scan_id] = index
-            
-        
-        # '''build main seg loader'''
-        # seg_dataloader = torch.utils.data.DataLoader(
-        #     dataset_seg, batch_size=1, num_workers=0,
-        #     shuffle=False, drop_last=False,
-        #     pin_memory=True,
-        #     collate_fn=graph_collate,
-        # )
-        
+
         '''start eval'''
-        
         self.model.eval()
         for index in tqdm(range(len(dataset_inst))):
         # for data_inst in seg_dataloader:
@@ -162,13 +164,13 @@ class Trainer_IMP(BaseTrainer):
             
             if scan_id_inst not in scanid2idx_seg:
                 #TODO: what should we do if missing scans?
+                raise RuntimeError('')
                 continue
             
             index_seg = scanid2idx_seg[scan_id_inst]
             data_seg  = dataset_seg.__getitem__(index_seg)
             
             assert data_seg['scan_id'] == data_inst['scan_id']
-            
             
             '''process seg'''
             eval_dict={}
@@ -178,6 +180,9 @@ class Trainer_IMP(BaseTrainer):
                 # Process data dictionary
                 data = self.process_data_dict(data_seg)
                 data_inst = self.process_data_dict(data_inst)
+                
+                
+                eval_UpperBound(data,data_inst)
                 
                 # Shortcuts
                 scan_id = data['scan_id']
@@ -194,7 +199,9 @@ class Trainer_IMP(BaseTrainer):
                 data_inst['node_edges'] = data_inst['node_edges'].t().contiguous()
                 
                 # check input valid
-                if node_edges_ori.ndim==1: continue
+                if node_edges_ori.ndim==1: 
+                    raise RuntimeError('')
+                    continue
                 
                 ''' make forward pass through the network '''
                 node_cls, edge_cls = self.model(**data)
@@ -224,11 +231,11 @@ class Trainer_IMP(BaseTrainer):
                             '''merge nodes'''
                             predictions = node_cls[inst2masks[inst]]# get all predictions on that instance
                             node_cls_pred = torch.softmax(predictions, dim=1).mean(dim=0)# averaging the probability
-                            merged_node_cls[mask_new][inst_valid_node_cls_indices] = node_cls_pred[seg_valid_node_cls_indices] # assign and ignor
+                            merged_node_cls[mask_new,inst_valid_node_cls_indices] = node_cls_pred[seg_valid_node_cls_indices] # assign and ignor
                             
                         else:
                             #TODO: handle missing inst
-                            merged_node_cls[mask_new][noneidx_node_cls] = 1.0
+                            merged_node_cls[mask_new,noneidx_node_cls] = 1.0
                             # merged_node_cls_gt[mask_new]=noneidx_node_cls
                             
                             
@@ -240,7 +247,7 @@ class Trainer_IMP(BaseTrainer):
                         merged_instance2idx[inst]=counter
                         predictions = node_cls[inst2masks[inst]]
                         node_cls_pred = torch.softmax(predictions, dim=1).mean(dim=0)                        
-                        merged_node_cls[counter][inst_valid_node_cls_indices] = node_cls_pred[seg_valid_node_cls_indices]
+                        merged_node_cls[counter,inst_valid_node_cls_indices] = node_cls_pred[seg_valid_node_cls_indices]
                         merged_node_cls_gt[counter] = inst_gt_cls[mask_old]
                         counter+=1
                 if ignore_missing:
@@ -251,8 +258,8 @@ class Trainer_IMP(BaseTrainer):
                 inst_mask2inst=merge_batch_mask2inst(inst_mask2instance)
                 
                 # build search list for GT edge pairs
-                inst_gt_pairs = set()
-                inst_gt_rel_dict = dict() # gt instance pair -> predicate label
+                inst_gt_pairs = set() # 
+                inst_gt_rel_dict = dict() # This collects "from" and "to" instances pair as key  -> predicate label
                 for idx in range(len(inst_gt_rel)):
                     src_idx, tgt_idx = data_inst['node_edges'][0,idx].item(),data_inst['node_edges'][1,idx].item()
                     src_inst_idx, tgt_inst_idx = inst_mask2inst[src_idx], inst_mask2inst[tgt_idx]
@@ -305,9 +312,9 @@ class Trainer_IMP(BaseTrainer):
                         #ignore same part
                         
                         edge_pds = torch.softmax(edge_pds,dim=1).mean(0)
-                        merged_edge_cls[counter][inst_valid_edge_cls_indices] = edge_pds
+                        merged_edge_cls[counter,inst_valid_edge_cls_indices] = edge_pds
                     else:
-                        merged_edge_cls[counter][noneidx_edge_cls] = 1.0
+                        merged_edge_cls[counter,noneidx_edge_cls] = 1.0
                         
                     merged_edge_cls_gt[counter] = inst_edge_cls
                     counter+=1
@@ -337,7 +344,7 @@ class Trainer_IMP(BaseTrainer):
         
         vis = self.visualize(eval_tool=eval_tool)
         eval_dict['visualization'] = vis
-        return eval_dict, eval_tool
+        return eval_dict, eval_tool, eval_UpperBound.eval_tool
     def sample(self, dataloader):
         pass
         

ssg/utils/util_eva.py
@@ -1,5 +1,6 @@
 import os,math,json,pathlib,torch
 import numpy as np
+# import ssg
 from codeLib.utils.plot_confusion_matrix import plot_confusion_matrix
 from collections import defaultdict
 # from utils.plot_confusion_matrix import plot_confusion_matrix
@@ -863,7 +864,7 @@ class EvalSceneGraph():
             
                 if self.k>0:
                     # top_k_predicate, top_k_obj = [], []
-                    self.top_k_obj += evaluate_topk_object(bobj_gts, obj_pds)
+                    self.top_k_obj += evaluate_topk_object(bobj_gts, obj_pds,k=self.k)
                     
                     if rel_pds is not None:
                         gt_edges = get_gt(bobj_gts, rel_gts, edge_indices, mask2inst, self.multi_rel_prediction)
@@ -992,6 +993,88 @@ class EvalSceneGraph():
             f.write(self.gen_text())
         return results
             
+class EvalUpperBound():
+    def __init__(self, node_cls_names, edge_cls_names,noneidx_node_cls,noneidx_edge_cls
+                 ,multi_rel:bool,topK:int, none_name:str):
+        if multi_rel:
+            raise NotImplementedError()
+
+        self.node_cls_names = node_cls_names
+        self.edge_cls_names = edge_cls_names
+        self.noneidx_node_cls=noneidx_node_cls
+        self.noneidx_edge_cls=noneidx_edge_cls
+        '''evaluate'''
+        self.eval_tool = EvalSceneGraph(node_cls_names, edge_cls_names,
+                                multi_rel_prediction=multi_rel,k=topK,
+                                save_prediction=True,
+                                none_name=none_name) 
+    def __call__(self,data_seg,data_inst):
+        # Shortcuts
+        scan_id = data_seg['scan_id']
+        # gt_cls = data['gt_cls']
+        # gt_rel = data_seg['gt_rel']
+        mask2seg = data_seg['mask2instance']
+        seg_node_edges = data_seg['node_edges']
+        # data_seg['node_edges'] = data_seg['node_edges'].t().contiguous()
+        seg2inst = data_seg.get('seg2inst',None)
+        
+        inst_mask2instance = data_inst['mask2instance']
+        inst_gt_cls = data_inst['gt_cls']
+        inst_gt_rel = data_inst['gt_rel']
+        inst_node_edges = data_inst['node_edges']
+        
+        '''get upper bound'''
+        # Convert to inst
+        seg_mask2inst=dict()
+        for mask, seg in mask2seg.items():
+            inst = seg2inst[seg] if seg2inst is not None else seg
+            seg_mask2inst[mask] = inst
+        
+        # collect inst in seg
+        seg_instance_set = set()
+        for _, inst in seg_mask2inst.items():
+            seg_instance_set.add(inst)
+            
+        # Find missing nodes
+        missing_nodes=list()
+        for mask, inst in inst_mask2instance.items():
+            if inst not in seg_instance_set:
+                missing_nodes.append(mask)
+        
+        # build seg inst key
+        seg_inst_pair_edges = set()
+        for idx in range(len(seg_node_edges)):
+            src, tgt = seg_node_edges[idx][0].item(), seg_node_edges[idx][1].item()
+            src, tgt = seg_mask2inst[src],seg_mask2inst[tgt]
+            seg_inst_pair_edges.add((src,tgt))
+        
+        # Find missing edges
+        missing_edges=list()
+        for mask in range(len(inst_node_edges)):
+            src, tgt = inst_node_edges[mask][0].item(), inst_node_edges[mask][1].item()
+            key = (src,tgt)
+            if key not in seg_inst_pair_edges:
+                missing_edges.append(mask)
+        
+        ''' Create Foo prdiction vector '''
+        # Nodes
+        node_pred = torch.nn.functional.one_hot(inst_gt_cls, len(self.node_cls_names)).float()
+        node_pred[missing_nodes] = 0
+        node_pred[missing_nodes,self.noneidx_node_cls] = 1.0
+        
+        # edges
+        edge_pred = torch.nn.functional.one_hot(inst_gt_rel, len(self.edge_cls_names)).float()
+        edge_pred[missing_edges] = 0
+        edge_pred[missing_edges,self.noneidx_edge_cls] = 1.0
+        
+        self.eval_tool.add([scan_id], 
+                          node_pred,
+                          inst_gt_cls, 
+                          edge_pred,
+                          inst_gt_rel,
+                          [inst_mask2instance],
+                          data_inst['node_edges'])
+
 if __name__ == '__main__':
     tt = EvaClassification(['1','2'], [0,1])
     pd=dict()