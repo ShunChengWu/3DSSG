d64c57e3abb66ecc0e6ab7398a2c61c1599ef0c3
shun-cheng <shuncheng.wu91@gmail.com>
2022-09-29 18:52:51 +0200

    update

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
-source ~/.bashrc
 false | conda create -n 2dssg pytorch=1.11 torchvision cudatoolkit=10.2 pyg -c pytorch -c pyg
-conda activate 2dssg
-python -m pip install -U wandb trimesh matplotlib h5py plyfile open3d onnxruntime tensorboard graphviz
+source ~/.bashrc; conda activate 2dssg; \
+    python -m pip install -U wandb trimesh matplotlib h5py plyfile open3d onnxruntime tensorboard graphviz
