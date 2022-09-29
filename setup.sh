aa744df982b8edb1ec043398892424c4db6f6813
shun-cheng <shuncheng.wu91@gmail.com>
2022-09-29 19:01:49 +0200

    fixbug

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+source ~/.bashrc
 false | conda create -n 2dssg pytorch=1.11 torchvision cudatoolkit=10.2 pyg -c pytorch -c pyg
-source ~/.bashrc; conda activate 2dssg; \
-    python -m pip install -U wandb trimesh matplotlib h5py plyfile open3d onnxruntime tensorboard graphviz
+conda activate 2dssg;
+python -m pip install -U wandb trimesh matplotlib h5py plyfile open3d onnxruntime tensorboard graphviz
